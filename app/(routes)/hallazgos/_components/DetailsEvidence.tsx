6e1ad6a94350f04931521268fb7215d9799a5215
Eduardo San Miguel <eduardo.sanmiguel.dev@gmail.com>
2026-09-22 10:30:42 -0600

    revert: victoria changes

--- app/(routes)/hallazgos/_components/DetailsEvidence.tsx
+++ app/(routes)/hallazgos/_components/DetailsEvidence.tsx
@@ -190,10 +190,9 @@ export default function DetailsTabs({
             <Divider />
 
             <Typography variant="caption" sx={sectionTitleSx}>
-              {/* PRIORIDAD Y TIEMPOS */}
-              TIEMPOS
+              PRIORIDAD Y TIEMPOS TIEMPOS
             </Typography>
-            {/* {priorityLabel !== "Sin prioridad" && (
+            {priorityLabel !== "Sin prioridad" && (
               <Box>
                 <Typography sx={rowLabelSx}>Prioridad</Typography>
                 <Typography variant="body2">{priorityLabel}</Typography>
@@ -207,7 +206,7 @@ export default function DetailsTabs({
                   </Typography>
                   <Typography variant="body2">{remainingDays}</Typography>
                 </Box>
-              )} */}
+              )}
             <Box>
               <Typography sx={rowLabelSx}>Fecha de registro</Typography>
               <Typography variant="body2">
